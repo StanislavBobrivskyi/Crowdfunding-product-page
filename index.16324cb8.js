1b038602f5e111a3d1fa67a1bcf9c43bee01bd8d
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-08-08 14:01:13 +0000

    Deploying to gh-pages from @ StanislavBobrivskyi/Crowdfunding-product-page@c0fbfcc783d8a696f0dd412378d6e8f74e2e5ea1 🚀

---
+++ index.16324cb8.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".menu"),n=document.querySelector("#mobileMenu");e.addEventListener("click",(function(){n.classList.toggle("is-hidden")}));
+//# sourceMappingURL=index.16324cb8.js.map
---
+++ index.e7f3e597.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".menu"),n=document.querySelector("#mobileMenu");e.addEventListener("click",(function(){n.classList.toggle("is-hidden")}))}();
+//# sourceMappingURL=index.e7f3e597.js.map
