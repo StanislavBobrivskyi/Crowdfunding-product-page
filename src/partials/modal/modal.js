2affbd12975a5054a9d1c92559c4c160d7e62ca0
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-25 10:27:39 +0200

    overflou modal

--- src/partials/modal/modal.js
+++ src/partials/modal/modal.js
@@ -1,14 +1,28 @@
 const modal = document.querySelector('.modal');
-
 const modalOpen = document.getElementById('openModalButton');
 const modalClose = document.getElementById('closeModalButton');
+const modalMenuOverlay = document.querySelector('.modal-menu-overlay');
 
 modalOpen.addEventListener('click', onClick);
 modalClose.addEventListener('click', onClose);
+modalMenuOverlay.addEventListener('click', onCloseOutside);
+
 function onClick() {
+  document.body.classList.add('modal-open');
+  modalMenuOverlay.classList.remove('is-hidden');
   modal.classList.remove('is-hidden');
 }
 
 function onClose() {
+  document.body.classList.remove('modal-open');
   modal.classList.add('is-hidden');
+  modalMenuOverlay.classList.add('is-hidden');
+}
+
+function onCloseOutside(event) {
+  if (!modal.contains(event.target) && !modalOpen.contains(event.target)) {
+    document.body.classList.remove('modal-open');
+    modal.classList.add('is-hidden');
+    modalMenuOverlay.classList.add('is-hidden');
+  }
 }
