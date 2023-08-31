961df2939a72e3211a9cab351dc8d5c11d65f9d3
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-31 14:38:50 +0200

    all modal box close

--- src/partials/modal/modal.js
+++ src/partials/modal/modal.js
@@ -4,10 +4,20 @@ const modalClose = document.getElementById('closeModalButton');
 const modalMenuOverlay = document.querySelector('.modal-menu-overlay');
 
 const checkbox = document.getElementById('checkbox');
+const checkbox1 = document.getElementById('checkbox1');
+const checkbox2 = document.getElementById('checkbox2');
+const checkbox3 = document.getElementById('checkbox3');
+
 const additionalWindow = document.getElementById('additionalWindow');
+const additionalWindow1 = document.getElementById('additionalWindow1');
+const additionalWindow2 = document.getElementById('additionalWindow2');
+const additionalWindow3 = document.getElementById('additionalWindow3');
+
 const successModal = document.getElementById('my-success-Modal');
 const continueBtn = document.getElementById('continue');
 const successClose = document.getElementById('success_close');
+
+const borderItem = document.querySelector('.card-item');
 
 modalOpen.addEventListener('click', onClick);
 modalClose.addEventListener('click', onClose);
@@ -41,6 +51,18 @@ checkbox.addEventListener('change', () => {
   }
 });
 
+checkbox1.addEventListener('change', () => {
+  additionalWindow1.style.display = checkbox1.checked ? 'block' : 'none';
+});
+
+checkbox2.addEventListener('change', () => {
+  additionalWindow2.style.display = checkbox2.checked ? 'block' : 'none';
+});
+
+checkbox3.addEventListener('change', () => {
+  additionalWindow3.style.display = checkbox3.checked ? 'block' : 'none';
+});
+
 continueBtn.addEventListener('click', onSuccess);
 
 function onSuccess() {
@@ -58,3 +80,7 @@ function onSuccessClose() {
   modalMenuOverlay.classList.add('is-hidden');
   location.reload();
 }
+
+document.querySelectorAll('#continue').forEach(btn => {
+  btn.addEventListener('click', onSuccess);
+});
